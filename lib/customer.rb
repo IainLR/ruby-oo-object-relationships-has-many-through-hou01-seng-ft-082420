ce12b8b088258969d44fc84cd8ddb556d05a9758
IainLR <iainrobertzon@gmail.com>
2020-08-27 21:55:31 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,29 @@
+require 'pry' 
 class Customer
-  
+  attr_accessor :name, :age 
+    @@all = []
+
+  def initialize (name, age)
+    @name = name 
+    @age = age 
+    self.class.all << self
+  end 
+  def self.all
+    @@all
+  end 
+
+  def new_meal (waiter, total, tip = 0)
+    Meal.new(waiter, self, total, tip)
+  end 
+
+  def meals
+    #binding.pry
+    Meal.all.select {|m| m.customer == self}
+  end 
+
+  def waiters
+    
+    meals.map {|meal| meal.waiter}
+    #binding.pry
+  end 
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
 class Waiter
+    attr_accessor :name, :years
+    @@all = [] 
 
+    def initialize (name, years)
+        @name = name 
+        @years = years 
+        self.class.all << self 
+    end 
+    def self.all
+        @@all
+    end 
+
+    def new_meal (customer, total, tip =0)
+        Meal.new(self, customer, total, tip)
+    end 
+
+    def meals 
+        Meal.all.select {|food| food.waiter == self}
+    end 
+
+    def best_tipper 
+        #binding.pry
+        #meals.map {|meal| meal.tip}
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+            meal_a.tip <=> meal_b.tip
+          end
+         
+          best_tipped_meal.customer
+    end 
 end
